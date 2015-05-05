807e0341dca372ce96eb72e7bdb5e81da82cd679
Aaron Mangum <potatochip@users.noreply.github.com>
2015-05-04 22:38:38 -0400

    goodnight

--- meatball.py
+++ meatball.py
@@ -5,18 +5,42 @@ from __future__ import division
 import sys
 import pandas as pd
 import numpy as np
-import statsmodels.api as sm
-import seaborn as sns
+import matplotlib.pyplot as plt
+
+from sklearn.cross_validation import train_test_split
+
+from sklearn.linear_model import LinearRegression
 
 
-def prep_sample():
-    df = pd.DataFrame.from_csv('processed.cleveland.data', header=-1,
-                               index_col=None)
-    features = df.drop['diagnosis', axis=1]
-    response = 'heart_disease'
-    return df, features, response
+def prep_dataset(dataset='processed.cleveland.data'):
+    '''
+    uses sample dataset from clevaland heart disease study if no dataset passed
+    '''
+    df = pd.DataFrame.from_csv(dataset, header=-1, index_col=None)
+    df.columns = ['age', 'sex', 'chest_pain', 'resting_bp', 'cholesterol',
+                  'blood_sugar', 'ecg', 'max_hr', 'exercise_induced_angina',
+                  'st_depression', 'slope', 'num_major_vessels', 'thal',
+                  'diagnosis']
+    df.index.names = ['patient']
+    df = df.convert_objects(convert_numeric=True)
+    df.dropna(inplace=True)
 
-def choose_estimator():
+    features = df.drop('diagnosis', axis=1)
+    response = df.diagnosis
+    return features, response
+
+
+def estimator_database():
+    columns = ['Accuracy', 'Precision', 'Recall', 'F1', 'AUC']
+    df = pd.DataFrame(columns=[columns])
+    return df
+
+
+def choose_multi_variable_estimator():
+    pass
+
+
+def choose_single_estimator():
     pass
 
 
@@ -25,7 +49,10 @@ def choose_variables():
 
 
 def linear_regression():
-    pass
+    # using R-squared for accuracy here
+    linear_reg = LinearRegression()
+    linear_reg.fit(features, response)
+    return linear_reg.score(features, response), linear_reg
 
 
 def k_nearest_neighbor():
@@ -36,14 +63,42 @@ def logistic_regression():
     pass
 
 
+def make_plot(X, y, model, X_classifier='cholesterol'):
+    X = X[X_classifier]
+    plt.scatter(X, y, color='black')
+    plt.plot(X, model.predict(X), color='blue', linewidth=3)
+    plt.xticks(())
+    plt.yticks(())
+    plt.show()
+
+
 def main():
-    pass
+    X_train, X_test, y_train, y_test = train_test_split(features, response)
+
+    estimator_df = estimator_database()
+    for estimator in estimators:
+        if estimator is linear_regression:
+            rsquared, model = linear_regression()
+            evaluation_metrics = [rsquared, 0, 0, 0, 0]
+            make_plot(X_test, y_test, model)
+        elif estimator is k_nearest_neighbor:
+            pass
+        elif estimator is logistic_regression:
+            pass
+        else:
+            raise ValueError('Unknown estimator: {0}'.format(estimator))
+        estimator_df.loc['linear regression'] = evaluation_metrics
+
 
 if __name__ == '__main__':
     try:
-        variables = sys.argv
+        sys.argv[1]
     except:
         estimators = [linear_regression, k_nearest_neighbor,
-                      logistic_regression]
-        dataset, features, response = prep_sample()
-    main(estimators)
+                  logistic_regression]
+        features, response = prep_dataset()
+    else:
+        estimators = sys.argv[1]
+        features = sys.argv[2]
+        response = sys.argv[3]
+    main()
--- workon_meatball.py
+++ workon_meatball.py
@@ -1,49 +1,108 @@
 
 # coding: utf-8
 
-# In[40]:
+# In[130]:
 
 import pandas as pd
 import numpy as np
+import statsmodels.api as sm
+
+get_ipython().magic(u'matplotlib inline')
+import matplotlib.pyplot as plt
 
 
-# In[46]:
+# In[110]:
 
 df = pd.DataFrame.from_csv('processed.cleveland.data', header=-1, index_col=None)
 
 
-# In[61]:
+# In[111]:
 
 df.columns = ['age', 'sex', 'chest_pain', 'resting_bp', 'cholesterol', 'blood_sugar', 'ecg', 'max_hr', 'exercise_induced_angina', 'st_depression', 'slope', 'num_major_vessels', 'thal', 'diagnosis']
 df.index.names = ['patient']
 df = df.convert_objects(convert_numeric=True)
+df.dropna(inplace=True)
 
 
-# In[65]:
+# In[112]:
 
 df.head()
 
 
-# In[66]:
+# In[133]:
 
-features = ['systolic_bp', 'tobacco_use', 'ldl_cholesterol',
-            'abdominal_adiposity', 'family_history', 'type_a',
-            'overall_obesity', 'alcohol_use', 'age']
-response = 'heart_disease'
+features = df.drop('diagnosis', axis=1)
+response = df.diagnosis
+features = sm.add_constant(features)
+from sklearn.cross_validation import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(features, response)
 
 
-# In[63]:
+# In[114]:
 
 from sklearn.linear_model import LinearRegression
 
 
-# In[75]:
+# In[148]:
 
 linear_reg = LinearRegression()
-linear_reg.fit(df[features], df.response)
+linear_reg.fit(features, response)
 
 
-# In[79]:
+# In[ ]:
 
-df*features
+
+
+
+# In[126]:
+
+linear_reg.score(features, response)
+
+
+# In[134]:
+
+model = sm.OLS(response, features)
+results = model.fit()
+results.summary()
+
+
+# In[136]:
+
+plt.scatter(features.resting_bp, response)
+
+
+# In[141]:
+
+fig, ax = plt.subplots()
+fig = sm.graphics.plot_fit(results, 1, ax=ax)
+ax.set_ylabel("Diagnosis")
+ax.set_xlabel("Resting BP")
+ax.set_title("Linear Regression")
+
+
+# In[164]:
+
+X_train, X_test, y_train, y_test = train_test_split(features, response)
+X = X_test['cholesterol']
+y = y_test
+plt.scatter(X, y, color='black')
+plt.plot(X, linear_reg.predict(X), color='blue', linewidth=3)
+plt.xticks(())
+plt.yticks(())
+plt.show()
+
+
+# In[160]:
+
+model.predict()
+
+
+# In[161]:
+
+model.predict(X).shape
+
+
+# In[ ]:
+
+
 
