483ecd5b5a506a0f02496b9891fbabf3573f19e6
Aaron Mangum <potatochip@users.noreply.github.com>
2015-05-05 20:42:26 -0400

    heading home

--- meatball.py
+++ meatball.py
@@ -7,15 +7,30 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import statsmodels.formula.api as smf
+from collections import defaultdict
 
 from sklearn.cross_validation import train_test_split
-
 from sklearn.grid_search import GridSearchCV
+from sklearn.metrics import classification_report
 
 from sklearn.linear_model import LinearRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
 
+from sklearn.learning_curve import learning_curve
+from sklearn.metrics import roc_curve
+from sklearn.metrics import auc
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+from sklearn.metrics import f1_score
 
-def prep_dataset(dataset='processed.cleveland.data'):
+def get_sample_dataset(dataset='processed.cleveland.data'):
     '''
     uses sample dataset from clevaland heart disease study if no dataset passed
     '''
@@ -26,6 +41,8 @@ def prep_dataset(dataset='processed.cleveland.data'):
                   'diagnosis']
     df.index.names = ['patient']
     df = df.convert_objects(convert_numeric=True)
+    # change diagnosis from 0-4 scale to just 0 or 1
+    df.diagnosis = df.diagnosis.apply(lambda x: 0 if x == 0 else 1)
     df.dropna(inplace=True)
 
     features = df.drop('diagnosis', axis=1)
@@ -33,8 +50,8 @@ def prep_dataset(dataset='processed.cleveland.data'):
     return features, response, df
 
 
-def estimator_database():
-    columns = ['Accuracy', 'Precision', 'Recall', 'F1', 'AUC', 'Best_Parameters']
+def create_estimator_database():
+    columns = ['Accuracy', 'Precision', 'Recall', 'F1', 'AUC', 'BestParams']
     df = pd.DataFrame(columns=[columns])
     return df
 
@@ -47,40 +64,149 @@ def choose_single_estimator():
     pass
 
 
-def choose_variables():
-    pass
+def classifier_full_report():
+    scores = ['accuracy', 'precision', 'recall', 'f1']
+    for score in scores:
+        print("Working on "+model_name)
+        print("# Tuning hyper-parameters for %s\n" % score)
+        clf = GridSearchCV(estimator, parameters, cv=10, scoring=score)
+        clf.fit(X_train, y_train)
+        print("Best parameters set found on development set:\n")
+        print(clf.best_params_)
+        print("\nGrid scores on development set:\n")
+        for params, mean_score, scores in clf.grid_scores_:
+            print("%0.3f (+/-%0.03f) for %r" % (mean_score, scores.std() * 2, params))
+        print("\nDetailed classification report:\n")
+        print("The model is trained on the full development set.")
+        print("The scores are computed on the full evaluation set.\n")
+        y_true, y_pred = y_test, clf.predict(X_test)
+        print(classification_report(y_true, y_pred))
+        print("\n")
 
 
-def linear():
+def grid_squid(estimator, parameters, X_train, X_test, y_train, y_test, model_name, tuning='accuracy'):
+    # runs all the parameters specified and returns the best model
+    # tuning tunes the hyper parameters to get the best score for that specific scoring test
+    if model_name == 'Linear Regression':
+        print("Working on "+model_name)
+        clf = GridSearchCV(estimator, parameters, cv=10)
+        clf.fit(X_train, y_train)
+        return clf
+    else:
+        score_tuning = defaultdict(int)
+        score_list = []
+        # scores = ['accuracy', 'precision', 'recall', 'f1']
+        scores = [tuning]
+        for score in scores:
+            # need to have this return auc as well
+            print("Working on "+model_name)
+            print("# Tuning hyper-parameters for %s\n" % score)
+            clf = GridSearchCV(estimator, parameters, cv=10, scoring=score)
+            clf.fit(X_train, y_train)
+            print("Best parameters set found on development set:\n")
+            print(clf.best_params_)
+            # print("\nGrid scores on development set:\n")
+            # for params, mean_score, scores in clf.grid_scores_:
+            #     print("%0.3f (+/-%0.03f) for %r" % (mean_score, scores.std() * 2, params))
+            print("\nDetailed classification report:\n")
+            print("The model is trained on the full development set.")
+            print("The scores are computed on the full evaluation set.\n")
+            y_true, y_pred = y_test, clf.predict(X_test)
+            print(classification_report(y_true, y_pred))
+            print("\n")
+            score_tuning[score] = clf.best_params_
+            score_list.append(clf.best_score_)
+        precision = precision_score(y_test, clf.predict(X_test))
+        score_list.append(precision)
+        recall = recall_score(y_test, clf.predict(X_test))
+        score_list.append(recall)
+        f1 = f1_score(y_test, clf.predict(X_test))
+        score_list.append(f1)
+        probabilities = clf.fit(X_train, y_train).predict_proba(X_test)
+        fpr, tpr, thresholds = roc_curve(y_test, probabilities[:, 1], pos_label=1)
+        roc_auc = auc(fpr, tpr)
+        score_list.append(roc_auc)
+        return score_list, score_tuning[tuning]
+
+
+def linear_foward_selection(X_train, y_train):
+    '''
+    forward selection of optimize adjusted R-squared by adding features that help
+    the most one at a time until the score goes down or you run out of features
+    not implemeneted yet
+    '''
+    remaining = {X_train.columns}
+    remaining.remove(response)
+    selected = []
+    current_score, best_new_score = 0.0, 0.0
+    while remaining and current_score == best_new_score:
+        scores_with_candidates = []
+        for candidate in remaining:
+            formula = "{} ~ {} + 1".format(response,
+                                           ' + '.join(selected + [candidate]))
+            score = smf.ols(formula, data).fit().rsquared_adj
+            scores_with_candidates.append((score, candidate))
+        scores_with_candidates.sort()
+        best_new_score, best_candidate = scores_with_candidates.pop()
+        if current_score < best_new_score:
+            remaining.remove(best_candidate)
+            selected.append(best_candidate)
+            current_score = best_new_score
+    formula = "{} ~ {} + 1".format(response,
+                                   ' + '.join(selected))
+    model = smf.ols(formula, data).fit()
+    return model
+
+
+def linear(X_train, X_test, y_train, y_test):
     '''
     linear regression. using R-squared for accuracy here
     '''
     linear_reg = LinearRegression()
-    parameters = {'normalize':(True, False)}
-    clf = GridSearchCV(linear_reg, parameters, n_jobs=-1)
-    clf.fit(features, response)
-    return clf.score(features, response), clf, clf.get_params()
+    parameters = {'normalize':[True, False]}
+    clf = grid_squid(linear_reg, parameters, X_train, X_test, y_train, y_test, 'Linear Regression')
+    return clf.score(X_test, y_test), clf, clf.best_params_
+    # implement foward selection for the number of variables
 
-
-def knn():
+def knn(X_train, X_test, y_train, y_test):
     '''
     K-nearest neighbor.
     '''
-    pass
+    knn = KNeighborsClassifier()
+    parameters = {'n_neighbors':range(1,50), 'weights':['uniform', 'distance'], 'algorithm':['ball_tree', 'kd_tree', 'brute', 'auto'], 'leaf_size':[10,20,30,40,50,60], 'p':[1,2]}
+    scores, best = grid_squid(knn, parameters, X_train, X_test, y_train, y_test, 'KNN')
+    return scores, best
 
 
-def logistic():
+def logistic(X_train, X_test, y_train, y_test):
     '''
     Logistic regression.
     '''
-    pass
+    log_reg = LogisticRegression()
+    parameters = {'penalty':['l1','l2'], 'solver':['liblinear','lbfgs','newton-cg']}
+    scores, best = grid_squid(log_reg, parameters, X_train, X_test, y_train, y_test, 'Logistic Regression')
+    return scores, best
+
+
+def gaussian(X_train, X_test, y_train, y_test):
+    '''
+    Gaussian NB
+    '''
+    gaussian = GaussianNB()
+    parameters = {}
+    scores, best = grid_squid(gaussian, parameters, X_train, X_test, y_train, y_test, 'Gaussian NB')
+    return scores, best
+
+
+def sample_size_learning_curve(model):
+    train_sizes, train_scores, test_scores = learning_curve(model, classifiers, response, cv=10)
+    plt.plot(train_sizes, np.mean(train_scores, axis=1), label='train')
+    plt.plot(train_sizes, np.mean(test_scores, axis=1), label='test')
+    plt.legend()
 
 
 def make_plot(X, y, model, test_data, model_name, response='diagnosis'):
     feature = X.columns
-    # X_choice = X[feature[5]]
-    # plt.scatter(X_choice, y, color='black')
-    # plt.plot(X_choice, model.predict(X), color='blue', linewidth=3)
     f, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2, sharey=False)
     sns.regplot(X[feature[4]], y, test_data, ax=ax1)
     sns.boxplot(X[feature[4]], y, color="Blues_r", ax=ax2)
@@ -116,23 +242,30 @@ def make_plot(X, y, model, test_data, model_name, response='diagnosis'):
     plt.savefig(model_name+'_coefficient_effects', bbox_inches='tight')
 
 
-
 def main():
     X_train, X_test, y_train, y_test, data_train, data_test = train_test_split(features, response, data)
 
-    estimator_df = estimator_database()
+    estimator_df = create_estimator_database()
     for estimator in estimators:
         if estimator is linear:
-            rsquared, model, parameters = linear()
+            rsquared, model, parameters = linear(X_train, X_test, y_train, y_test)
             evaluation_metrics = [rsquared, 0, 0, 0, 0, parameters]
-            make_plot(X_test, y_test, model, data_test, model_name='linear', response='diagnosis')
+            if plots: make_plot(X_test, y_test, model, data_test, model_name='linear', response='diagnosis')
         elif estimator is knn:
-            print 
+            scores, best = knn(X_train, X_test, y_train, y_test)
+            accuracy, precision, recall, f1, auc = scores
+            evaluation_metrics = [accuracy, precision, recall, f1, auc, best]
         elif estimator is logistic:
-            pass
+            scores, best = logistic(X_train, X_test, y_train, y_test)
+            accuracy, precision, recall, f1, auc = scores
+            evaluation_metrics = [accuracy, precision, recall, f1, auc, best]
+        elif estimator is gaussian:
+            scores, best = gaussian(X_train, X_test, y_train, y_test)
+            accuracy, precision, recall, f1, auc = scores
+            evaluation_metrics = [accuracy, precision, recall, f1, auc, best]
         else:
             raise ValueError('Unknown estimator: {0}'.format(estimator))
-        estimator_df.loc['linear regression'] = evaluation_metrics
+        estimator_df.loc[estimator] = evaluation_metrics
     print estimator_df
 
 
@@ -140,10 +273,13 @@ if __name__ == '__main__':
     try:
         sys.argv[1]
     except:
-        estimators = [linear, knn, logistic]
-        features, response, data = prep_dataset()
+        estimators = [linear, knn, logistic, gaussian]
+        features, response, data = get_sample_dataset()
+        plots = False
     else:
         estimators = sys.argv[1]
         features = sys.argv[2]
         response = sys.argv[3]
+        data = sys.arvg[4]
+        plots = sys.argv[5]
     main()
--- workon_meatball.py
+++ workon_meatball.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[321]:
+# In[529]:
 
 import pandas as pd
 import numpy as np
@@ -12,25 +12,26 @@ get_ipython().magic(u'matplotlib inline')
 import matplotlib.pyplot as plt
 
 
-# In[322]:
+# In[530]:
 
 df = pd.DataFrame.from_csv('processed.cleveland.data', header=-1, index_col=None)
 
 
-# In[323]:
+# In[531]:
 
 df.columns = ['age', 'sex', 'chest_pain', 'resting_bp', 'cholesterol', 'blood_sugar', 'ecg', 'max_hr', 'exercise_induced_angina', 'st_depression', 'slope', 'num_major_vessels', 'thal', 'diagnosis']
 df.index.names = ['patient']
 df = df.convert_objects(convert_numeric=True)
+df.diagnosis = df.diagnosis.apply(lambda x: 0 if x == 0 else 1)
 df.dropna(inplace=True)
 
 
-# In[324]:
+# In[532]:
 
 df.head()
 
 
-# In[389]:
+# In[494]:
 
 features = df.drop('diagnosis', axis=1)
 response = df.diagnosis
@@ -81,7 +82,7 @@ ax.set_xlabel("Resting BP")
 ax.set_title("Linear Regression")
 
 
-# In[239]:
+# In[495]:
 
 X_train, X_test, y_train, y_test = train_test_split(features, response)
 X = X_test
@@ -138,11 +139,6 @@ sns.residplot(X.cholesterol, (linear_reg.predict(X) - y) ** 2, color="indianred"
 X.cholesterol
 
 
-# In[285]:
-
-(linear_reg.predict(X.cholesterol) - y) ** 2
-
-
 # In[165]:
 
 from sklearn.grid_search import GridSearchCV
@@ -158,43 +154,33 @@ sns.interactplot(X[X.columns[4]], X[X.columns[5]], y.values, ax=ax4, cmap="coolw
 y.values
 
 
-# In[199]:
+# In[504]:
 
 from sklearn.svm import SVC
 linear_reg = LinearRegression()
+parameters = {'normalize':(True, False)}
 
 
-# In[203]:
+# In[505]:
 
 parameters = {'normalize':(True, False)}
 clf = GridSearchCV(linear_reg, parameters, n_jobs=-1)
 clf.fit(features,response)
 
 
-# In[206]:
+# In[510]:
 
 clf.score(features,response)
 
 
-# In[178]:
+# In[511]:
 
-svr = SVC()
-parameters = {'kernel':('linear', 'rbf'), 'C':[1, 10]}
-
-
-# In[194]:
-
-clf = GridSearchCV(svr, parameters, n_jobs=-1)
+linear_reg
 
 
-# In[195]:
+# In[509]:
 
-clf.fit(features, response)
-
-
-# In[214]:
-
-clf.get_params()
+clf.get_params
 
 
 # In[198]:
@@ -217,7 +203,171 @@ ax.grid(False)
 ax.set_title='Multi-variable correlation significance level'
 
 
-# In[400]:
+# In[401]:
 
 sns.coefplot("diagnosis ~ "+' + '.join(features), df, intercept=True, palette="Set1")
+
+
+# In[442]:
+
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+
+
+# In[ ]:
+
+log_reg = LogisticRegression()
+log_reg
+
+
+# In[404]:
+
+import matplotlib
+import mpld3
+from mpld3 import plugins, utils
+
+
+# In[469]:
+
+# X = features
+# y = response
+
+# # dither the data for clearer plotting
+# X += 0.1 * np.random.random(X.shape)
+
+# fig, ax = plt.subplots(4, 4, sharex="col", sharey="row", figsize=(8, 8))
+# fig.subplots_adjust(left=0.05, right=0.95, bottom=0.05, top=0.95,
+#                     hspace=0.1, wspace=0.1)
+
+# for i in range(4):
+#     for j in range(4):
+#         points = ax[3 - i, j].scatter(X[[j]], X[[i]], s=40, alpha=0.6)
+
+# # remove tick labels
+# for axi in ax.flat:
+#     for axis in [axi.xaxis, axi.yaxis]:
+#         axis.set_major_formatter(plt.NullFormatter())
+
+# # Here we connect the linked brush plugin
+# plugins.connect(fig, plugins.LinkedBrush(points))
+
+# mpld3.display()
+
+
+# In[ ]:
+
+
+
+
+# In[512]:
+
+# n_neighbors=5, weights='uniform', algorithm='auto', leaf_size=30, p=2, metric='minkowski', metric_params=None
+parameters = {'n_neighbors':range(1,50)}
+knn = KNeighborsClassifier()
+
+
+# ValueError: 'Accuracy_weighted' is not a valid scoring value. Valid options are
+# ['accuracy', 'adjusted_rand_score', 'average_precision', 'f1', 'f1_macro', 'f1_micro', 
+#  'f1_samples', 'f1_weighted', 'log_loss', 'mean_absolute_error', 'mean_squared_error', 
+#  'median_absolute_error', 'precision', 'precision_macro', 'precision_micro', 
+#  'precision_samples', 'precision_weighted', 'r2', 'recall', 'recall_macro', 
+#  'recall_micro', 'recall_samples', 'recall_weighted', 'roc_auc']
+
+scores = ['accuracy', 'precision_weighted', 'recall_weighted', 'f1_weighted']
+for score in scores:
+    print("# Tuning hyper-parameters for %s" % score)
+    print()
+    
+    clf = GridSearchCV(knn, parameters, cv=5, scoring=score)
+    clf.fit(X_train, y_train)
+
+    print("Best parameters set found on development set:")
+    print()
+    print(clf.best_params_)
+    print()
+    print("Grid scores on development set:")
+    print()
+    for params, mean_score, scores in clf.grid_scores_:
+        print("%0.3f (+/-%0.03f) for %r"
+              % (mean_score, scores.std() * 2, params))
+    print()
+
+    print("Detailed classification report:")
+    print()
+    print("The model is trained on the full development set.")
+    print("The scores are computed on the full evaluation set.")
+    print()
+    y_true, y_pred = y_test, clf.predict(X_test)
+    print(classification_report(y_true, y_pred))
+    print()
+
+
+# In[516]:
+
+clf.best_score_
+
+
+# In[476]:
+
+from __future__ import print_function
+
+from sklearn import datasets
+from sklearn.cross_validation import train_test_split
+from sklearn.grid_search import GridSearchCV
+from sklearn.metrics import classification_report
+from sklearn.svm import SVC
+
+print(__doc__)
+
+# Loading the Digits dataset
+digits = datasets.load_digits()
+
+# To apply an classifier on this data, we need to flatten the image, to
+# turn the data in a (samples, feature) matrix:
+n_samples = len(digits.images)
+X = digits.images.reshape((n_samples, -1))
+y = digits.target
+
+# Split the dataset in two equal parts
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.5, random_state=0)
+
+# Set the parameters by cross-validation
+tuned_parameters = [{'kernel': ['rbf'], 'gamma': [1e-3, 1e-4],
+                     'C': [1, 10, 100, 1000]},
+                    {'kernel': ['linear'], 'C': [1, 10, 100, 1000]}]
+
+scores = ['precision', 'recall']
+
+for score in scores:
+    print("# Tuning hyper-parameters for %s" % score)
+    print()
+
+    clf = GridSearchCV(SVC(C=1), tuned_parameters, cv=5,
+                       scoring='%s_weighted' % score)
+    clf.fit(X_train, y_train)
+
+    print("Best parameters set found on development set:")
+    print()
+    print(clf.best_params_)
+    print()
+    print("Grid scores on development set:")
+    print()
+    for params, mean_score, scores in clf.grid_scores_:
+        print("%0.3f (+/-%0.03f) for %r"
+              % (mean_score, scores.std() * 2, params))
+    print()
+
+    print("Detailed classification report:")
+    print()
+    print("The model is trained on the full development set.")
+    print("The scores are computed on the full evaluation set.")
+    print()
+    y_true, y_pred = y_test, clf.predict(X_test)
+    print(classification_report(y_true, y_pred))
+    print()
 
